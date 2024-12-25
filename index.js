72e061c5e8ba7b15fbf03dc13c453febb5b24f0d
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2024-12-25 19:03:37 -0300

    att

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const exphbs = require('express-handlebars')
 const livros = require('./livros/livro');
-
+const port = process.env.PORT || 3001
 
 app.engine('handlebars', exphbs.engine()); 
 app.set('view engine', 'handlebars'); 
@@ -13,6 +13,6 @@ app.get('/',(req,res)=>{
     res.render('home')
 })
 
-app.listen(4000,()=>{
+app.listen(port,()=>{
     console.log('Servidor Rodando')
 })
