fb3c02e85e01af5e8be5f4308a6c32aaced2529e
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2024-12-27 20:12:55 -0300

    att

--- db/conn.js
+++ db/conn.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2');
 
 // Criação da conexão com base nas variáveis de ambiente
 const connection = mysql.createConnection({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'blibioteca',
+
 });
 
 // Testar a conexão
